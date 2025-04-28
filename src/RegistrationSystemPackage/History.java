fe269934705c569d3d5c0536c3c241e720b7214b
Youssef <youssef24p@gmail.com>
2025-04-28 19:02:55 +0300

    finish history

--- src/RegistrationSystemPackage/History.java
+++ src/RegistrationSystemPackage/History.java
@@ -52,6 +52,32 @@ class History {
 
     void redo() {
         // Remember to print about what done (add, remove and etc)
+
+        int studentId=0, courseId=0;
+        boolean print = true;
+
+        if (!redoStack.isEmpty()) {
+            String action = redoStack.pop();
+            undoStack.push(action);
+
+            List<String> actionList = Arrays.asList(action.split(" "));
+            studentId = Integer.parseInt(actionList.get(1));
+            courseId = Integer.parseInt(actionList.get(2));
+            print = Boolean.parseBoolean(actionList.get(3));
+
+            if (actionList.get(0).equals("addStudentToCourse")) {
+                regSystem.addStudentToCourse(studentId, courseId, print, false);
+            } else if (actionList.get(0).equals("removeStudentFromCourse")) {
+                regSystem.removeStudentFromCourse(studentId, courseId, print, false);
+            } else {
+                System.out.println("action is not valid");
+            }
+
+        } else {
+            System.out.println("there no action to redo");
+        }
+
+
     }
 
 
