28cb2b7372d26155a46d8fb9e2bf186571b737b2
Youssef <youssefmail1456@gmail.com>
2025-05-01 04:24:54 +0100

    some code to test (not important)

--- src/DataStructuresProject/DataStructuresProject.java
+++ src/DataStructuresProject/DataStructuresProject.java
@@ -20,11 +20,26 @@ public class DataStructuresProject {
             mySystem1.addStudent(1002);
             mySystem1.addStudent(1003);
             mySystem1.addStudent(1004);
+            mySystem1.addStudent(1005);
+            mySystem1.removeStudent(1001);
             mySystem1.addCourse(54);
             mySystem1.addCourse(55);
             mySystem1.addCourse(56);
-            mySystem1.removeStudent(1001);
+            mySystem1.addCourse(57);
+            mySystem1.addCourse(58);
             mySystem1.removeCourse(54);
+
+            mySystem1.addStudentToCourse(1002, 56);
+            mySystem1.addStudentToCourse(1002, 55);
+            mySystem1.addStudentToCourse(1002, 57);
+            mySystem1.addStudentToCourse(1003, 57);
+            mySystem1.addStudentToCourse(1004, 57);
+
+            mySystem1.displayStudentsOfCourse(57);
+            mySystem1.displayStudentsOfCourse(55);
+            mySystem1.displayStudentsOfCourse(333);
+            mySystem1.displayStudentsOfCourse(58);
+
 
             
             
