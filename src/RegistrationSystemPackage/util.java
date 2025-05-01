2ac201a00b52cd46977d372a176f70d3b572488e
Youssef <youssefmail1456@gmail.com>
2025-05-01 18:17:58 +0100

    Add selection sort

--- src/RegistrationSystemPackage/util.java
+++ src/RegistrationSystemPackage/util.java
@@ -4,9 +4,41 @@ package RegistrationSystemPackage;
 class util {
 
     static void sort(int[] arr) {
-        countingSort(arr);
+
+
+        // Check which algorithm is more fast
+        int n = arr.length;
+        int k = arr[0];
+        for (int num : arr) {
+            if (num > k) {
+                k = num;
+            }
+        }
+
+        if (n+k <= n*n) {
+            countingSort(arr); // O(n+k)
+        } else {
+            selectionSort(arr); // O(n^2)
+        }
     }
 
+    // O(n^2)
+    private static void selectionSort(int[] arr) {
+        int outer, inner, min;
+        for (outer = 0; outer < arr.length - 1; outer++) {
+            min = outer;
+            for (inner = outer + 1; inner < arr.length; inner++) {
+                if (arr[inner] < arr[min]) {
+                    min = inner;
+                }
+            }
+            int temp = arr[outer];
+            arr[outer] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    // O(n+k)
     private static void countingSort(int[] arr) {
         if (arr.length == 0) return;
 
